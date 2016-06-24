90e4e9df8a796da3e236732506570af4eae31d33
jyunnie <jyunnie@github.com>
2016-06-24 15:13:27 +0900

    added initial source file

---
+++ hash.cu
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<cuda_gl_interop.h>
+#define N 1024
+#define Blocksize 16
+
+__global__ void calculate_hash(int *a,int *b,int *c,int N)
+{
+	
+}
+
+__global__ void 
+
+int main()
+{
+
+}
+
+
