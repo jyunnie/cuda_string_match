ea30d1d34e5c5ff5ce7dc9903ef02bc69e8f83eb
jyunnie <jyunnie@github.com>
2016-06-29 22:12:39 +0900

    added parameters

--- string_match.cu
+++ string_match.cu
@@ -1,77 +1,121 @@
-include<stdio.h>
+#include<stdio.h>
+#include<string.h>
 #include<stdlib.h>
+#include<unistd.h>
+#include<cuda_gl_interop.h>
 #define N 2048
 #define Blocksize 16
+#define TOTAL_LINES 50000
+//#include "kernel_calculation.cu"
 
+//cuda memory allocation
+char *cuda_lines;
+char *cuda_wholedb;
+unsigned int obj_size = TOTAL_LINES * sizeof(char);
+unsigned int wholedb = 1000000 * sizeof(char);
+char *cuda_output;
+//unsigned int 
 
+char output[N*N];
+//FILES
+FILE *fp;
+FILE *db;
+FILE *result;
+char *st[4096],*st_db[10000000];
+char line[511];
+char dbline[511];
+
+void cuda_init();
+void calculation();
+void cleanup();
 
 int main()
 {
-  FILE *fp;
-  int i,*input_file_index;
-  char ch;
-  
+  cudaSetDevice(0);
+  int i = 0;//
+  int j= 0;
+
   fp=fopen("InputAnnotation.txt","r");
-  while(!feof(fp))
+   if(fp != NULL)
     {
-      ch = fgetc(fp);
-      if(ch == '\n')
+      char line[255];
+      while(fgets(line,sizeof line,fp))
 	{
+	  st[i]=strdup(line);// seperated lines in st[]
 	  i++;
 	}
-    }
-  input_file_index = (int *)malloc(sizeof(i));
+  fclose(fp);
+  db=fopen("10mdbsnp.txt","r");
+  if(db != NULL)
+    {
+      char line[255];
+      while(fgets(dbline,sizeof dbline,db))
+	{
+	  st_db[j]=strdup(dbline);// seperated dblines in st_db[]
+	  j++;
+	}
+  fclose(db)
+  cuda_init();
+  calculation();
+  atexit(cleanup);
+  cudaThreadExit();
   
+  return 0;
 }
 
-/*void fgetdata()
-{
-  //sprintf(FILENAME,"%s%.3d%s",__FILENAME, FILE_NUM, FILETYPE);
-  
-  if ((fp = fopen(FILENAME, "rb")) == NULL) {
-    puts(FILENAME);
-    puts("file open error!!");
-  }
-  
-  fread(DATA_NUM, sizeof(int), 1, fp);
-  printf("FILENAME: %s, DATA_NUM[0]: %d\n",FILENAME,DATA_NUM[0]);	
-  fread(st, sizeof(int), DATA_NUM[0] * 6, fp); 
-  for(int j=0; j<=DATA_NUM[0]*6;j++)
-    {
-      chr[j] = st[j*6];
-      start[j] = st[j*6+1];
-      end[j]= st[j*6+2];
-      ref[j] = st[j*6+3];
-      alt[j] = st[j*6+4];
-    }	
-  fclose(fp);
-  
-  }*/
-
-void memory_init(int lines){
+void cuda_init(){
   int deviceCount;
   cudaGetDevice(&deviceCount);
-  cudaMalloc((void**) &d_row);
-  cudaMalloc((void**) &d_T);
+  cudaMalloc((void**) &cuda_lines , obj_size);
+  cudaMalloc((void**) &cuda_wholedb,wholedb);
+  cudaMalloc((void**) &cuda_output,obj_size);
 }
 
 void calculation()
 {
   int deviceCount;
-  dim3 grid();
-  dim3 threads();
-  fgetdata();
+  dim3 grid(16,2);
+  dim3 threads(8,8);
+  
   cudaGetDevice(&deviceCount);
-  cudaMemset();
-  cudaMemcpy(cudaMemcpyHostToDevice);
-  string_match<<< grid,threads >>>(parameters);
+  cudaMemset(cuda_lines,0,obj_size);
+  cudaMemset(cuda_wholedb,0,wholedb);
+  cudaMemset(cuda_output,0,obj_size);
+  cudaMemcpy(cuda_lines,st,obj_size,cudaMemcpyHostToDevice);
+  cudaMemcpy(cuda_wholedb,st_db,wholedb,cudaMemcpyHostToDevice);
+
+  string_match<<< grid,threads >>>(cuda_lines,cuda_wholedb,cuda_output);
  
-  cudaMemcpy(cudaMemcpyDeviceToHost);
-  sprintf(FILENAME,write);
-  fwrite();
-  fp=fopen();
-  fclose(fp);
+  cudaMemcpy(output,cuda_output,obj_size,cudaMemcpyDeviceToHost);
+  if((result = fopen("Output_Annotation.txt","wb")) == NULL){
+      printf("FILE OPEN ERROR!! _WRITE");
+      exit(1);
+  }
+  fwrite(output,sizeof(char),2048*2048),result);
+ 
+  fclose(result);
 }
+
+void cleanup()
+{
+	printf("cleanup\n");
+	//---------------------------------------------
+	
+	cudaFree(cuda_lines);
+	cudaFree(cuda_wholedb);
+	cudaFree(cudaoutput);
+}
+
+__global__ void calculation(char *cuda_lines,char *cuda_wholedb,char cuda_output)
+{
+
+  unsigned int x = blockIdx.x*blockDim.x + threadIdx.x;
+  unsigned int y = blockIdx.y*blockDim.y + threadIdx.y;
+
+
+
+
+
 
 
 
